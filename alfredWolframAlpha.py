f76c961672b4bca27d3664c882c178b1e9d69d52
hiptechboy <PapaYaw@MacBook-Pro-5.local>
2015-11-25 13:21:44 -0500

    Change way API Key is handled
    Access API key via local file

--- alfredWolframAlpha.py
+++ alfredWolframAlpha.py
@@ -2,20 +2,22 @@ import wolframalpha
 
 class WolframAlpha(object):
     def __init__(self):
-        self.client = wolframalpha.Client('EWXH7L-THV8PX7UQG')
+        apiKeyFile = open('WolframAlphaKey.txt', 'r')
+        apiKey = apiKeyFile.read()
+        self.client = wolframalpha.Client('apiKey')
 
-    def wolframAlphaQuery(self, query): # Function adapted from baljeetsinghipu in Rasberry Pi forums http://goo.gl/pSmNMW
-        res = self.client.query(query)
-        if len(res.pods) > 0:
+    # Function adapted from baljeetsinghipu in Rasberry Pi forums http://goo.gl/pSmNMW
+    def wolframAlphaQuery(self, query): 
+        answer = self.client.query(query)
+        if len(answer.pods) > 0:
             texts = ""
-            pod = res.pods[1]
+            pod = answer.pods[1]
             if pod.text:
                 texts = pod.text
             else:
                 texts = pronounce("I have no answer for that")
-            # to skip ascii character in case of error
-            result = texts.encode('ascii', 'ignore')
-            return str(result)
+            # Skip ascii character in case of error
+            answer = texts.encode('ascii', 'ignore')
+            return str(answer)
         else:
             return "Sorry, I am not sure."
-    
